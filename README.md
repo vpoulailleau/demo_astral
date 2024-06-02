878e8f5e0297c790e87c11c18a48b1db512f72bc
Vincent Poulailleau <vpoulailleau@gmail.com>
2024-06-02 12:11:59 +0200

    continue demonstration

--- README.md
+++ README.md
@@ -6,7 +6,21 @@ This repo is forked from Django.
 
 ## Installation
 
+Install:
+
+- Python 3.12
+- `uv`: https://github.com/astral-sh/uv
+
 ```sh
 cp -R . ../demo_old
 cp -R . ../demo_new
+```
+
+## Demo
+
+Open 2 terminals, only the first line changes.
+
+```sh
+cd ../demo_old
+cat alias.sh
 ```
---
+++ alias.sh
@@ -0,0 +1,18 @@
+if [[ "${PWD##*/}" == *old* ]]; then
+
+    echo "Installing old aliases"
+    alias venv='python3.12 -m venv venv'
+    alias act='source venv/bin/activate'
+    alias lint='flake8'
+    alias format='black'
+
+else
+
+    echo "Installing new aliases"
+    alias venv='uv venv -p 3.12 venv'
+    alias act='source venv/bin/activate'
+    alias pip='uv pip'
+    alias lint='ruff check --select "ALL" --preview --output-format concise'
+    alias format='ruff format'
+
+fi
